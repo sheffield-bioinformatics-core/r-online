5356f1f382ab0ef9c34a9f112f154a0fa6d204bf
markdunning <mark.dunning@gmail.com>
2020-07-15 18:33:09 +0100

    Formatting and typo checking
    use stylesheets for colouring etc
    
    add pointer to data carpentry
    
    modify filtering exercise to make more challenging
    
    change boxplot examples so not to use year

---
+++ check_packages.R
@@ -0,0 +1,23 @@
+if (require(dplyr,quietly = TRUE)) {
+  message("The dplyr package has been installed")
+} else {
+  message("The dplyr package has NOT been installed. Please try typing the command 'install.packages('dplyr')' again")
+}
+if (require(ggplot2,quietly = TRUE)) {
+  message("The ggplot2 package has been installed")
+} else {
+  message("The ggplot2 package has NOT been installed. Please try typing the command 'install.packages('ggplot2')' again")
+}
+ 
+if (require(readr,quietly = TRUE)) {
+  message("The readr package has been installed")
+} else {
+    message("The readr package has NOT been installed. Please try typing the command 'install.packages('readr')' again")
+} 
+
+if (all(require(readr,quietly = TRUE) & require(dplyr,quietly = TRUE) & require(ggplot2,quietly = TRUE))){
+  message("You have successfully installed all the packages required for the course")
+} else{
+  message("One of more of the required packages have not been installed. Please check the above messages and re-install the missing packages")
+}
+  
